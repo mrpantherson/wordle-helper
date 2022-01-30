69d7ba6349494eb8a28a60b41117ffa1f0f50d19
brandon <26782348+mrpantherson@users.noreply.github.com>
2022-01-30 13:59:06 -0500

    swapped dict for the actual word list the puzzles uses

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import os
 import wordle
 
-path = os.path.join('../data', 'dict.txt')
-word = wordle.Wordle(path)
+path = os.path.join('../data', 'dict-wordle.txt')
+word = wordle.Wordle(path, export_lexicon=False)
+
+word.term_frequency()
