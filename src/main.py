c6b9e998eef7e6e99871e65afaecd815d2a2c87b
Brandon <oldmanpatterson@gmail.com>
2022-02-23 10:14:09 -0500

    first commit on wordle game implementation

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
 import os
 import wordle
+import wordle_game as game
+
 
 path = os.path.join('../data', 'dict-wordle.txt')
 cpath = os.path.join('../data', 'dict-common.txt')
-word = wordle.Wordle(path, cpath, export_lexicon=False)
 
-word.term_frequency()
+word = wordle.Wordle(path, cpath, export_lexicon=False)
+game = game.WordleGame('sharp')
+
+for i in range(6):
+    guess_word = word.freq[-1]
+    result = game.guess(guess_word)
+
+    if result == 'ggggg':
+        print(f'you won with {guess_word} in {game.guesses} tries!')
+        break
+
+    word.guess(guess_word, result)
---
+++ src/wordle_game.py
@@ -0,0 +1,33 @@
+"""
+
+"""
+
+
+class WordleGame:
+
+    """
+    """
+
+    def __init__(self, secret_word):
+        """
+        """
+        self.secret_word = secret_word
+        self.guesses = 0
+
+    def reset(self):
+        self.guesses = 0
+
+    def guess(self, guess):
+        """"
+        """
+        results = []
+        for g, c in zip(guess, self.secret_word):
+            if g == c:
+                results.append('g')
+            elif g in self.secret_word:
+                results.append('y')
+            else:
+                results.append('b')
+
+        self.guesses += 1
+        return ''.join(results)
